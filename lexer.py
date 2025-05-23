2198abf36238bfc7b69982331f096b26e329b0cd
Jesus-0502 <jesusgutierrez4825@gmail.com>
2025-05-22 22:30:16 -0400

    Juntar utils en lexer

--- lexer.py
+++ lexer.py
@@ -1,8 +1,19 @@
 import sys
 import os
 import ply.lex as lex
-from utils import find_column, print_error
+#from utils import find_column, print_error
 from tokens import *
+
+
+def find_column(input_text, token):
+    last_cr = input_text.rfind('\n', 0, token.lexpos)
+    if last_cr < 0:
+        last_cr = -1
+    column = token.lexpos - last_cr
+    return column
+
+def print_error(char, row, column):
+    print(f"Error: Unexpected character \"{char}\" in row {row}, column {column}")
 
 lexer = lex.lex()
 
